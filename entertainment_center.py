8583be22112439667716eeb9c63784ba6031e13f
Bhaskar Dua <bhaskar_dua@yahoo.com>
2016-12-05 01:59:11 +0800

    docs: Added comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,7 @@
 import media
 import fresh_tomatoes
 
+# Create instances of each movie to be displayed
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
@@ -31,5 +32,8 @@ hunger_games = media.Movie("Hunger Games",
                            "https://upload.wikimedia.org/wikipedia/en/4/4b/Hunger_Games_Film_Poster.jpg",
                            "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
+# Create a list of the movie instances
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+# Generate html file and open in web browser
 fresh_tomatoes.open_movies_page(movies)
